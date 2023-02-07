0b76a189b7c69e045502f5e4bb14e4a8bb70a96e
Efthimis Charitonidis <efthimis@charitonidis.xyz>
2023-02-07 18:35:01 +0200

    Initial commit

---
+++ fresh_install_windows.ps1
@@ -0,0 +1,32 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+
+if ( -Not (Get-Command choco.exe -ErrorAction SilentlyContinue)) {
+    # install chocolatey
+    iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+
+$packagesToInstall = 
+'visualstudio2022community',
+'vscode',
+'heidisql',
+'dotnet-sdk',
+'postman',
+'7zip',
+'microsoft-edge',
+'notepadplusplus',
+'git',
+'curl',
+'office-tool',
+'mp3tag',
+'transmission',
+'vlc',
+'imageresizerapp',
+'recuva',
+'Gpg4win';
+
+ForEach ($package in $packagesToInstall) {
+    iex 'choco install $package -y';
+}
+
+# Install Office LTSC 2021
+setup /download configuration.xml
